058854133ddc3c56b09e8afc53daf39b978ce3af
Michael Bernards <michael.bernards@mail.de>
2023-10-16 14:38:18 +0200

    dir copy

--- fs.go
+++ fs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
@@ -18,7 +19,10 @@ func (c *Context) copyTarget(t *Target) error {
 			return fmt.Errorf("error stat file %s: %v", e, err)
 		}
 		if fi.IsDir() {
-			fmt.Printf("copy dir %s\n", e)
+			_, err := c.copydir(e, t)
+			if err != nil {
+				fmt.Printf("error copying dir: %v\n", err)
+			}
 		} else {
 			_, err := c.copyfile(e, filepath.Join(t.Path, fi.Name()))
 			if err != nil {
@@ -32,8 +36,38 @@ func (c *Context) copyTarget(t *Target) error {
 	return nil
 }
 
+func (c *Context) copydir(src string, t *Target) (int, error) {
+	parent := filepath.Dir(src)
+	count := 0
+	err := filepath.Walk(src, func(path string, info fs.FileInfo, err error) error {
+		subpath := path[len(parent):]
+		targetpath := filepath.Join(t.Path, subpath)
+		if info.IsDir() {
+			fmt.Printf("create dir %s\n", targetpath)
+			err := os.MkdirAll(targetpath, info.Mode())
+			if err != nil {
+				return fmt.Errorf("error creating dir %s: %v", targetpath, err)
+			}
+		} else {
+			_, err := c.copyfile(path, targetpath)
+			if err != nil {
+				return fmt.Errorf("error copy file %s to %s: %v", path, targetpath, err)
+			}
+			count++
+		}
+		return nil
+	})
+	return count, err
+}
+
+// copyfile copies one file
+// src absolute path of the file be be copied
+// dst absolute path including filename of the copy
+// returns number of bytes copied and an error if oone occured
 func (c *Context) copyfile(src string, dst string) (int, error) {
-	fmt.Printf("copy file %s to %s\n", src, dst)
+	if c.Verbose {
+		fmt.Printf("copy file %s to %s\n", src, dst)
+	}
 	bs, err := toByteSize(c.Buffersize)
 	if err != nil {
 		return 0, err
@@ -66,6 +100,9 @@ func (c *Context) copyfile(src string, dst string) (int, error) {
 		}
 		copiedBytes += n
 	}
-	fmt.Printf("copied %d bytes\n", copiedBytes)
+	if c.Verbose {
+		fmt.Printf("copied %d bytes\n", copiedBytes)
+	}
+
 	return copiedBytes, nil
 }
